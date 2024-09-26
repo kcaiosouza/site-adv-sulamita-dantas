058343ac2c979e8d76fb9995927393bcace84c11
Caio Souza <kcaiosouza@gmail.com>
2024-09-26 00:49:44 -0300

    :sparkles: feat: Página dashboard com validação via SSR

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,50 @@
+import { firestore } from "@/services/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSideProps, GetServerSidePropsContext } from "next"
+import { parseCookies, destroyCookie } from 'nookies';
+
+interface User {
+	email: string;
+	lastName: string;
+	name: string;
+	type: string;
+}
+
+export default function Dashboard() {
+	return (
+		<main>
+			<span>DASHBOARD</span>
+		</main>
+	)
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+	const {'sulaadv.AuthToken': token} = parseCookies(ctx);
+
+	if(!token) {
+		return {
+			redirect: {
+				destination: '/login',
+				permanent: false,
+			}
+		}
+	}
+
+	const docRef = doc(firestore, "Users", token);
+	const docSnap = await getDoc(docRef)
+	if(docSnap.exists()){
+		const userInfo = docSnap.data() as User
+		if(userInfo.type == "user"){
+			return {
+				redirect: {
+					destination: '/blog',
+					permanent: false,
+				}
+			}
+		}
+	}
+
+	return {
+		props: {}
+	}
+}
